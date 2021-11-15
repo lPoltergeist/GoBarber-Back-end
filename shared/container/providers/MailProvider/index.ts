7e223586d8e3472624814a94594b0e3ecc4e7a31
Gabriel José <72321620+lPoltergeist@users.noreply.github.com>
2021-11-15 10:24:56 -0300

    Add files via upload

---
+++ shared/container/providers/MailProvider/index.ts
@@ -0,0 +1,16 @@
+import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
+
+import IMailProvider from './models/IMailProvider';
+import EtherealMailProvider from './implementations/EtherealMailProvider';
+import SESMailProvider from './implementations/SESMailProvider';
+
+const providers = {
+  ethereal: EtherealMailProvider,
+  ses: SESMailProvider,
+};
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  container.resolve<IMailProvider>(providers[mailConfig.driver]),
+);
